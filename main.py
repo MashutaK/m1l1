b2b623e61a0e2a74056f8c0083dbd00ca6c76c60
MashutaK <146443258+MashutaK@users.noreply.github.com>
2023-10-13 19:22:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное",
-            "РОФЛ": "шутка",
-            "ЩИЩ" : "одобрение или восторг",
-            "КРИПОВЫЙ" : "страшный, пугающий" ,
-            "АГРИТЬСЯ" : "злиться"
-            }
-            
-word = input("Введите непонятное слово (большими буквами!): ")
-if word in meme_dict.keys():
-    print(meme_dict[word])
-    
-else:
-    print('Такое слово не найдено!')
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=discord.Intents.default())
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def mood(ctx):
+    await ctx.send(f"{bot.user} is happy!")
+
+bot.run("MTE1OTg4MDExNjM2MTUxMTA0Nw.GGPFvX.Hma_bAK976spiMSRE4bvBbJC3l-yQeEA0ucsyY")
